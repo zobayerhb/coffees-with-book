cb0f7c7121cb9a9a9b1ded607b0a19c073540057
zobayerhb <hosenzobayer55@gmail.com>
2024-11-12 21:13:36 +0600

    something modified

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -19,7 +19,7 @@ const CategoryCard = () => {
     }
   }, [category, data]);
 
-  console.log(coffees);
+  console.log(category);
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
